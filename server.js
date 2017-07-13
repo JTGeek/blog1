63126f8a5487b393352994f5eab99806546df787
James Timberlake <James.Timberlake.1018@gmail.com>
2017-07-12 20:03:54 -0400

    2nd commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const blogPostsRouter = require('./blogPostsRouter');
+
+
+// log the http layer
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+app.use('/blog-posts', blogPostsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+
+app.listen(process.env.PORT || 9000, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 9000}`);
+});
