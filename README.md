bdd9727bf7753bc181836fe15cc0268c92b08843
James Timberlake <James.Timberlake.1018@gmail.com>
2017-08-12 03:21:57 -0400

    removed router.js, updated server, config and models.js. Added seed-data.json (db seed).

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 
 # mlab user login:  blogPoster: blogPassWord1
+
+
+
+To do: Models: Mongoose//////done
+To do: blogPostsRouter: Mongoose//////replaced with updated server.js based off resturant app example.
+To do: Test, seed 
+
+**Do get and posts first, then test.  
--- config.js
+++ config.js
@@ -1,4 +1,14 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     'mongodb://blogPoster:blogPassWord1@ds113063.mlab.com:13063/jtdb';
-exports.PORT = process.env.PORT || 8080;
+
+
+// exports.TEST_DATABASE_URL = (
+//     process.env.TEST_DATABASE_URL ||
+//     'mongodb://localhost/blogdb');
+
+
+exports.PORT = process.env.PORT || 9000;
+
+
+//mongoimport --db jtdb --collection blogPosts --drop --file ~/Desktop/Projects/blog/seed-data.json --host ds113063.mlab.com --port 113063  -u blogPoster -p blogPassWord1
